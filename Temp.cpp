07eb516c8ab8f8a624f5e3a7eb35a357ee26c16f
Solirous <99412371+Solirous@users.noreply.github.com>
2022-11-29 19:22:45 +0700

    Create Temp.cpp

---
+++ Temp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define f(i,n) for(i;i<n;i++)
+#define vec_int vector<int>
+using namespace std;
+int t;
+void slv(){
+	
+	
+	
+}
+main(){
+	cin.tie(0)->sync_with_stdio(0);
+	bool tc =true;
+	if(tc){
+		cin>>t;
+		while(t--)
+			slv();
+		
+	}
+	
+}
